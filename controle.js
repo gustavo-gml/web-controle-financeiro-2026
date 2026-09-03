dfc4252fa3e36ee8ecfc909705b69bbcfa1c1e72
gustavo-gml <gustavomartinslima06@gmail.com>
2026-09-03 16:34:38 -0300

    feat: adiciona funcionalidade de adição e remoção de transações com identificação de receita e despesa

--- controle.js
+++ controle.js
@@ -26,7 +26,7 @@ form.addEventListener("submit", (e) => {
     e.preventDefault(); //sequestra o evento padrão de envio
 
     const descTransacao = descInput.value;
-    const valorTransacao = valorInput.value;
+    let valorTransacao = Math.abs(valorInput.value);
 
     // Fail Fast: sempre que mexer com api externa, valide tudo logo no 
     // inicio do método, antes de qualquer coisa
@@ -39,15 +39,24 @@ form.addEventListener("submit", (e) => {
     descInput.value = "";
     valorInput.value = "";
 
+    const botaoSubmit = document.activeElement; 
+    const idDoBotao = botaoSubmit.id;
+
+    
+    if (idDoBotao == "btn-despesa"){
+        valorTransacao = -valorTransacao
+    }
     //objeto literal
     const transacao = {
-        id: parseInt(Math.random() * 1000),
+        id: parseInt(gerarProximoID()),
         descricao: descTransacao,
         valor: parseFloat(valorTransacao)
     }
 
+    
+
     somaAoSaldo(transacao);
-    somaReceitaDespesa(transacao);
+    somaReceitaDespesa(transacao, idDoBotao);
     addTransacaoAoDOM(transacao);
 
     transacoesSalvas.push(transacao);
@@ -61,17 +70,20 @@ function addTransacaoAoDOM(transacao) {
 
     const li = document.createElement('li');
     li.innerHTML = `${transacao.descricao}<span>R$ ${transacao.valor}</span><button onClick="excluiTransacao(${transacao.id})"class="delete-btn">X</button>`
+    li.id = "transacao-" + transacao.id
     li.classList.add(classe)
 
     transacoesUl.append(li)//não pode innerhtml
 }
-function somaReceitaDespesa(transacao) {
+function somaReceitaDespesa(transacao, idBtn) {
 
-    const elemento = transacao.valor > 0 ? receitaP : despesaP
-    const substituir = transacao.valor > 0 ? "+ R$" : "- R$";
+    const elemento = idBtn == "btn-receita" ? receitaP : despesaP
+    const substituir = idBtn == "btn-receita" ? "+ R$" : "- R$";
     let valorAtual = elemento.innerHTML.replace(substituir, "")
+
     valorAtual = parseFloat(valorAtual)
     valorAtual += Math.abs(transacao.valor)
+
     elemento.innerHTML = valorAtual
     elemento.innerHTML = `${substituir}${valorAtual.toFixed(2)}`
 
@@ -79,15 +91,30 @@ function somaReceitaDespesa(transacao) {
 }
 
 function somaAoSaldo(transacao) {
-    const valorTransacao = transacao.valor; // pegamos para não mudar o valor inicial
+    const valorTransacao = transacao.valor; 
 
-    let total = balancoH1.innerHTML.replace('R$', ''); //não é o input (é uma tag), por isso o innerhtml
-    // .replace é um método da string que substitui
-    
+    let total = balancoH1.innerHTML.replace('R$', ''); 
     total = parseFloat(total);
     total += valorTransacao;
     
     balancoH1.innerHTML = `R$${total.toFixed(2)}`;
+}
+
+function desfazerTransacoes(transacao, idBtn) {
+    let total = balancoH1.innerHTML.replace('R$', '');
+
+    total = parseFloat(total);
+    total -= transacao.valor;
+    balancoH1.innerHTML = `R$${total.toFixed(2)}`;
+
+    const elemento = idBtn == "btn-receita" ? receitaP : despesaP
+    const substituir = idBtn == "btn-receita" ? "+ R$" : "- R$";
+    let valorAtual = elemento.innerHTML.replace(substituir, "")
+
+    valorAtual = parseFloat(valorAtual)
+    valorAtual -= Math.abs(transacao.valor)
+
+    elemento.innerHTML = `${substituir}${valorAtual.toFixed(2)}`;
 }
 
 
@@ -97,9 +124,13 @@ function carregarDados() {
     receitaP.innerHTML = '+ R$0.00'
     despesaP.innerHTML = '- R$0.00'
 
+    let operacao;
     for (let i = 0; i < transacoesSalvas.length; i++) {
         somaAoSaldo(transacoesSalvas[i]);
-        somaReceitaDespesa(transacoesSalvas[i]);
+
+        operacao = transacoesSalvas[i].valor > 0 ? "btn-receita" : "btn-despesa";
+        somaReceitaDespesa(transacoesSalvas[i], operacao);
+
         addTransacaoAoDOM(transacoesSalvas[i]);
     }
 }
@@ -107,10 +138,31 @@ function carregarDados() {
 carregarDados();
 
 function excluiTransacao(id){
+
     const transacaoIndex = transacoesSalvas.findIndex((transacao) => transacao.id == id); //percorre o vetor todo executando uma função
 
-    transacoesSalvas.splice(transacaoIndex, 1);
+    const transacaoRemovida = transacoesSalvas[transacaoIndex];
+    const operacao = transacaoRemovida.valor > 0 ? "btn-receita" : "btn-despesa";
 
+    
+    desfazerTransacoes(transacaoRemovida,operacao)
+
+
+    const elementoDOM = document.getElementById("transacao-" + id);
+    
+    if (elementoDOM) {
+            elementoDOM.remove();
+        }
+
+    transacoesSalvas.splice(transacaoIndex, 1); 
     localStorage.setItem(chave_transacoes_ls, JSON.stringify(transacoesSalvas));
-    carregarDados();
+
+}
+
+function gerarProximoID(){
+
+    if(transacoesSalvas.length == 0)
+        return 1
+    
+    return  transacoesSalvas[transacoesSalvas.length - 1].id + 1
 }
